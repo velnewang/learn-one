5689c4bcfeb20cae3d101f883107af0377b19913
Velne Wang <velnewang@gmail.com>
2018-10-11 15:17:50 +0800

    ADD: Inference.java

---
+++ javawork/cksimulator/src/main/java/ck2/bean/Inference.java
@@ -0,0 +1,14 @@
+package ck2.bean;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import ck2.bean.character.Person;
+import ck2.bean.military.Army;
+
+public class Inference {
+
+    public LinkedHashSet<Person> persons;
+    public Set<Army> armies;
+
+}
