f94c9f921fdade8032b52d63f5a7b7b4645d6091
Velne Wang <velnewang@gmail.com>
2018-10-11 15:14:57 +0800

    ADD: military.Army

---
+++ javawork/cksimulator/src/main/java/ck2/bean/military/Army.java
@@ -0,0 +1,11 @@
+package ck2.bean.military;
+
+public class Army {
+
+    public int id;
+    public String name;
+    public Branch[] branchs;
+    public int[] arrange;
+    public int location;
+
+}
