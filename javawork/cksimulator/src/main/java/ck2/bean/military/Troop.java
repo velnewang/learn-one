ee9615c1abe613290d5d7f16dc61ad387407500d
Velne Wang <velnewang@gmail.com>
2018-10-11 15:14:28 +0800

    ADD: military.Troop

---
+++ javawork/cksimulator/src/main/java/ck2/bean/military/Troop.java
@@ -0,0 +1,12 @@
+package ck2.bean.military;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Troop {
+
+    public int home;
+    public Date date;
+    public Map<String, Integer> squads;
+
+}
